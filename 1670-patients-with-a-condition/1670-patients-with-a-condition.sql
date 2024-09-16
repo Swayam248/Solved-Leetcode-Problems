4a2eae16b770d2bac4add985f5afd428b3dcf9e2
Swayam Patnaik <108750468+Swayam248@users.noreply.github.com>
2024-09-16 09:40:55 +0530

    Time: 311 ms (72.31%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
+-- select patient_id, patient_name, conditions from patients WHERE conditions like '%DIAB1%'
 select patient_id, patient_name, conditions from patients WHERE conditions REGEXP '\\bDIAB1';
+#This regular expression searches for DIAB1 as a whole word.
+#\\b denotes a word boundary, ensuring that DIAB1 is not part of a larger word.
+#This handles cases where DIAB1 is at the start, end, or surrounded by spaces in the conditions string.
