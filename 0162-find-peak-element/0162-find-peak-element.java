9655737f09a6f626d080d41ff114f97fc456083a
Swayam Patnaik <108750468+Swayam248@users.noreply.github.com>
2024-09-17 21:48:40 +0530

    Time: 0 ms (100%), Space: 42.3 MB (42.21%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,8 +1,11 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        // for single element, return 0
         if(nums.length == 1){
             return 0;
         }
+
+        // for 2 elements, check which one is greater and return that index
         if(nums.length == 2){
             if(nums[0] > nums[1]){
                 return 0;
@@ -11,20 +14,25 @@ class Solution {
                 return 1;
             }
         }
+
+        //check if the first element is greater
         if(nums[0]>nums[1]){
             return 0;
         }
         
+        //check if the last element is greater
         if(nums[nums.length-1] > nums[nums.length-2]){
             return nums.length-1;
         }
-         int c = 0;
-         for(int i = 1;i<nums.length-1;i++){
+
+        //middle element check
+        int c = 0;
+        for(int i = 1;i<nums.length-1;i++){
             if(nums[i] > nums[i+1] && nums[i] > nums[i-1]){
                 return i;
             }
-         }
-         return c;
+        }
+        return c;
     }
 }
 
