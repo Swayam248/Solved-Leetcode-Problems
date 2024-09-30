1f07416822e9a8c6cc88ef490c5bdb07df4a3d9a
Swayam Patnaik <108750468+Swayam248@users.noreply.github.com>
2024-09-30 12:21:52 +0530

    Time: 14 ms (64.77%), Space: 11.8 MB (35.37%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,22 +5,7 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # x=0
-        # gl = list(s)
-        # l=[]
-        # for i in t:
-        #     if i in s:
-        #         l.append(i)
-        #         x+=1
-        #         continue
-        # if len(s) == x and l == gl:
-        #     return True
-        # else:
-        #     return False
-            # Pointers for s and t
         i, j = 0, 0
-    
-    # Iterate over t
         while i < len(s) and j < len(t):
         # If characters match, move the pointer of s forward
             if s[i] == t[j]:
@@ -28,7 +13,7 @@ class Solution(object):
         # Always move the pointer of t forward
             j += 1
     
-    # If we have traversed all characters of s, return True
+        # If we have traversed all characters of s, return True
         return i == len(s)
             
         
