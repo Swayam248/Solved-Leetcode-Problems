22c93e7bc6119a07e5e2e1658defcf7bfc559fdc
Swayam Patnaik <108750468+Swayam248@users.noreply.github.com>
2025-05-02 15:39:33 +0530

    Time: 0 ms (100%), Space: 25.5 MB (92.11%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,12 +1,6 @@
-class Solution(object):
-    def rotate(self, nums, k):
-        n = len(nums)
-        k = k % n  # In case k > n (like rotating 10 times on an array of size 5)
-        # Because rotating an array of size 7 by 10 steps is the same as rotating it by 10 % 7 = 3 steps.
-        nums[:] = nums[-k:] + nums[:-k]
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
+class Solution:
+    def rotate(self,nums, k):
+        n=len(nums)
+        k=k%n
+        nums[:]=nums[-k:]+nums[:-k]
         
