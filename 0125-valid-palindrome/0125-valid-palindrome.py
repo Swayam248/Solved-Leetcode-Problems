a433b736438e72908f2bf30e0b157cd870c0a1a6
Swayam Patnaik <108750468+Swayam248@users.noreply.github.com>
2025-05-03 00:37:03 +0530

    Time: 4 ms (90.77%), Space: 18.4 MB (43.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,36 +1,4 @@
-# class Solution(object):
-#     def isPalindrome(self, s):
-#         """
-#         :type s: str
-#         :rtype: bool
-#         """
-#         j=-1
-#         flag = 0
-#         s= s.replace(" ","")
-#         punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-#         for ele in s:
-#             if ele in s:
-#                 s = s.replace(ele, "")
-#         for i in range ((len(s)//2)):
-#             if(s[i] == s[j]):
-#                 j-=1
-#                 continue
-#             else:
-#                 flag = 1
-#                 break
-#         if (flag == 0):
-#             return True
-#         else:
-#             return False
-        
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        # Remove all non-alphanumeric characters and convert to lowercase
-        cleaned_string = ''.join(char.lower() for char in s if char.isalnum())
-        
-        # Check if the cleaned string is equal to its reverse
-        return cleaned_string == cleaned_string[::-1]
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = "".join([ch for ch in s if ch.isalnum()]).lower()
+        return res == res[::-1]
