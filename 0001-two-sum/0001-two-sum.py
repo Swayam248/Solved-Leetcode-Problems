aadd09835cfdbc4b85faa7bd9212f43db90d9cb6
Swayam Patnaik <108750468+Swayam248@users.noreply.github.com>
2025-05-04 12:34:15 +0530

    Time: 3608 ms (5%), Space: 18.5 MB (79.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,10 +3,10 @@ class Solution:
         l=[]
         for i in range(len(nums)):
             for j in range(len(nums)):
-                if nums[i] + nums[j] == target and i!=j:
+                if nums[i] + nums[j] == target and i!=j: #i!=j as we cannot add same element(case 2 - 3+3 is not allowed)
                     l.append(i)
                     l.append(j)
                     break
-            if len(l) == 2:
+            if len(l) == 2:  #already 2 elements in list? solved. break
                 break
         return l
